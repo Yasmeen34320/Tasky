c300cc12a772632635ddb94d74d32320db95de88
Yasmeen Alaa <y.alaa9.2001@gmail.com>
2025-05-30 00:49:47 +0300

    layout

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -10,6 +11,33 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SizedBox(height: 52),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SvgPicture.asset(
+                    'assets/images/arrow.svg',
+                    width: 30,
+                    height: 30,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(width: 4),
+                Text(
+                  'Completed Tasks',
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
